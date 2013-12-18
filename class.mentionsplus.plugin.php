7f940e037aeb3163c42e7b812dffa24cd80fb698
R-J <robin.jurinka@gmx.net>
2013-12-18 11:06:43 +0100

    Update class.mentionsplus.plugin.php
    Added setting section for /me action

--- class.mentionsplus.plugin.php
+++ class.mentionsplus.plugin.php
@@ -17,7 +17,8 @@ class MentionsPlusPlugin extends Gdn_Plugin {
    public function Setup() {
     // Set config settings only if they are not already set
       if (!C('Garden.User.ValidationRegex')) {
-         SaveToConfig('Garden.User.ValidationRegex', '\d\w_ äöüß'); // special German characters for testing
+         // numbers, letters a to z, space and special German characters
+         SaveToConfig('Garden.User.ValidationRegex', '\d\w_ äöüß');
       }
       if (!C('Garden.User.ValidationLength')) {
          SaveToConfig('Garden.User.ValidationLength', '{3,20}');
@@ -28,13 +29,17 @@ class MentionsPlusPlugin extends Gdn_Plugin {
       if (!C('Plugins.MentionsPlus.MentionStop')) {
          SaveToConfig('Plugins.MentionsPlus.MentionStop', '"');
       }
+      if (!C('Plugins.MentionsPlus.MeActionCode')) {
+         SaveToConfig('Plugins.MentionsPlus.MeActionCode', '/me');
+      }
    }
 
+/*
    public function __construct() {
       require_once(PATH_PLUGINS.DS.'MentionsPlus'.DS.'class.umlautmentionsformatter.php');
       Gdn::FactoryInstall('MentionsFormatter', 'UmlautMentionsFormatter', NULL, Gdn::FactoryInstance);
    }
-   
+*/   
    
    public function SettingsController_MentionsPlus_Create($Sender) {
       $Sender->Permission('Garden.Settings.Manage');
@@ -44,30 +49,36 @@ class MentionsPlusPlugin extends Gdn_Plugin {
       $Conf = new ConfigurationModule($Sender);
       $Conf->Initialize(array(
          'Garden.User.ValidationRegex' => array(
-            'LabelCode' => 'Regular expression that evaluates a valid username',
+            'LabelCode' => 'Letters allowed in user name',
             'Control' => 'TextBox',
-            'Default' => '\d\w_äöüß'
+            'Default' => '\d\w_ äöüß',
+            'Description' => T('Settings ValidationRegex Description', 'Regular expression that evaluates a valid username (<a href="http://www.php.net/manual/en/ref.pcre.php">regex</a>)')
          ),
          'Garden.User.ValidationLength' => array(
-            'LabelCode' => 'Min/max length for valid usernames (in regex notation)',
+            'LabelCode' => 'Min/max length of user names',
             'Control' => 'TextBox',
-            'Default' => '{3,20}'
+            'Default' => '{3,20}',
+            'Description' => T('Settings ValidationLength Description', 'Minimum and maximum length of user names in <a href="http://www.php.net/manual/en/ref.pcre.php">regex</a> notation')
          ),
          'Plugins.MentionsPlus.MentionStart' => array(
             'LabelCode' => 'Beginning escape character',
             'Control' => 'TextBox',
             'Default' => '"',
-            'Description' => T('SettingMentionStartDescription', 'If using whitespaces in usernames you have to mark what belongs to a username: @"hans wurst"')
+            'Description' => T('Settings MentionStart Description', 'If using whitespaces in usernames you have to mark what belongs to a username: @"hans wurst".<br /><strong>Enter only one single character!</strong>')
          ),
          'Plugins.MentionsPlus.MentionStop' => array(
             'LabelCode' => 'Ending escape character',
             'Control' => 'TextBox',
             'Default' => '"',
-            'Description' => T('SettingMentionStopDescription', 'If you would like to use different characters for escaping, you could set them separately: @<hans wurst>')
+            'Description' => T('Settings MentionStop Description', 'If you would like to use different characters for escaping, you could set them separately: @{hans wurst}.<br /><strong>Enter only one single character!</strong>')
+         ),
+         'Plugins.MentionsPlus.MeActionCode' => array(
+            'LabelCode' => '/me action code',
+            'Control' => 'TextBox',
+            'Default' => '/me',
+            'Description' => T('Settings MeActionCode Description', 'By default "/me" is not translatable. In theory, you could set it to "->ich" or anything you like with this setting.<br /><strong>This feature is not tested at all!</strong>')
          )
       ));
       $Conf->RenderAll();
    }
 }
-
-
